fa0b1c2cdc7b4b5c29c9a79162cdce867231bedf
Pankaj Yadav <pankaj2005ydv10@gmail.com>
2026-04-07 13:22:36 +0530

    loops

---
+++ loops/loops.py
@@ -0,0 +1,16 @@
+for i in range(1,5):
+    print(i)
+    
+name="Pankajyadav"
+for i in name:
+    print(i)
+    
+for i in range(1,11):
+    print("5 X",i,"=",5*i)
+    
+i=1    
+while(i<7):
+    print(i)
+    i=i+1
+
+
